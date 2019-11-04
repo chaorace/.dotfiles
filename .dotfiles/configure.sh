911e331ce4bfe0925c8063284f4cc76911baf5ae
chaorace@gmail.com <chaorace@gmail.com>
2019-11-04 07:43:51 -0500

    Switch to using /home/chao variable

--- .dotfiles/configure.sh
+++ .dotfiles/configure.sh
@@ -13,8 +13,8 @@ systemctl --user enable mbsync.timer
 systemctl --user start mbsync.timer
 
 #Load Doom Emacs package
-git clone -b develop https://github.com/hlissner/doom-emacs $HOME/.emacs.d
-$HOME/.emacs.d/bin/doom refresh
+git clone -b develop https://github.com/hlissner/doom-emacs "$HOME/.emacs.d"
+"$HOME/.emacs.d/bin/doom" refresh
 
 #Recache Fonts
 fc-cache
